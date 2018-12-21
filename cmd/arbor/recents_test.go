1c4b06b32e155565df0097a9eb2af15040cff922
aThorp96 <andrewThorp@protonmail.com>
2018-12-20 21:25:17 -0500

    flesh out tests

--- cmd/arbor/recents_test.go
+++ cmd/arbor/recents_test.go
@@ -28,36 +28,93 @@ func TestRecentListConstructor(t *testing.T) {
 	g.Expect(data).To(gomega.BeEmpty())
 }
 
-func TestRecentListRemoveParent(t *testing.T) {
+// TestRecemtListRemoveParentVacancy tests that a parent message is replaced when
+// a its child is added to the RecentList, before the queue is full
+func TestRecentListRemoveParentVacancy(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
-	r1, _ := NewRecents(5)
-	r2, _ := NewRecents(5)
+	r, err := NewRecents(5)
+	if err != nil {
+		t.Skip("Failed to create RecentList", err)
+	}
 
-	m0, _ := NewChatMessage("message 0")
+	m0, err := NewChatMessage("message 0")
+	if err != nil {
+		t.Skip("Failed to create message")
+	}
 	m0.AssignID()
-	r1.Add(m0)
-	r2.Add(m0)
+	r.Add(m0)
 
-	// r1 contains m1 with no parent ID
-	// r2 contains m1 with m0 as parent
-	// so m0 should be removed from recents
-	m1, _ := NewChatMessage("message 1")
+	m1, err := m0.Reply("message1")
+	if err != nil {
+		t.Skip("Failed to reply to message")
+	}
 	m1.AssignID()
-	r1.Add(m1)
-	m1.Parent = m0.UUID
-	r2.Add(m1)
-	g.Expect(r1.Data()).Should(gomega.ContainElement(m0.UUID))
-	g.Expect(r2.Data()).ShouldNot(gomega.ContainElement(m0.UUID))
 
-	//m2, _ := NewChatMessage("message 2")
-	//m3, _ := NewChatMessage("message 3")
-	//m4, _ := NewChatMessage("message 4")
-	//m5, _ := NewChatMessage("message 5")
-	//m6, _ := NewChatMessage("message 6")
-	//m7, _ := NewChatMessage("message 7")
-	//m8, _ := NewChatMessage("message 8")
+	for i := 0; i < 7; i++ {
+		r.Add(m1)
+		g.Expect(r.Data()).ShouldNot(gomega.ContainElement(m0.UUID))
+		g.Expect(r.Data()).Should(gomega.ContainElement(m1.UUID))
+		m0 = m1
+		m1, err := m0.Reply("new message")
+		if err != nil {
+			t.Skip("Failed to reply to message")
+		}
+		m1.AssignID()
+	}
 }
 
+// TestRecemtListRemoveParentFull tests that a parent message is replaced when
+// a its child is added to the RecentList, after the queue is full
+func TestRecentListRemoveParentFull(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	r, err := NewRecents(5)
+	if err != nil {
+		t.Skip("Failed to create RecentList", err)
+	}
+
+	// Fill the queue with root messages
+	for i := 0; i < 5; i++ {
+		m1, err := NewChatMessage("new message")
+		if err != nil {
+			t.Skip("Failed to reply to message")
+		}
+		m1.AssignID()
+		r.Add(m1)
+	}
+
+	// Create new root message for replying
+	m0, err := NewChatMessage("message 0")
+	if err != nil {
+		t.Skip("Failed to create message")
+	}
+	m0.AssignID()
+	r.Add(m0)
+
+	// First reply
+	m1, err := m0.Reply("message1")
+	if err != nil {
+		t.Skip("Failed to reply to message")
+	}
+	m1.AssignID()
+
+	// Spin up a bunch of replies
+	for i := 0; i < 7; i++ {
+		r.Add(m1)
+		g.Expect(r.Data()).ShouldNot(gomega.ContainElement(m0.UUID))
+		g.Expect(r.Data()).Should(gomega.ContainElement(m1.UUID))
+		// Shift messages down the queue
+		m0 = m1
+		m1, err := m0.Reply("new message")
+		if err != nil {
+			t.Skip("Failed to reply to message")
+		}
+		m1.AssignID()
+	}
+}
+
+// TestRecentListAddsNewMessages ensure that before and after the queue is
+// full, the newest message is ALWAYS in the list of recents, and that it
+// always replaces the oldest message if the parent is not in the RecentList.
 func TestRecentListAddsNewMessages(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	r, err := NewRecents(5)
@@ -116,4 +173,43 @@ func TestRecentListAddsNewMessages(t *testing.T) {
 	g.Expect(r.Data()).Should(gomega.ContainElement(m8.UUID))
 	g.Expect(r.Data()).Should(gomega.ContainElement(m4.UUID))
 	g.Expect(r.Data()).ShouldNot(gomega.ContainElement(m3.UUID))
+}
+
+// TestWorstCase tests that new messages are being inserted at the head of the
+// queue, even when they replace their parents.
+func TestWorstCase(t *testing.T) {
+	// create a list
+	g := gomega.NewGomegaWithT(t)
+	r, err := NewRecents(3)
+	g.Expect(err).To(gomega.BeNil())
+	g.Expect(r).ToNot(gomega.BeNil())
+
+	m0, _ := NewChatMessage("message 0")
+	m0.AssignID()
+	r.Add(m0)
+
+	// fill the list
+	m1, _ := NewChatMessage("message 1")
+	m1.AssignID()
+	r.Add(m1)
+	m2, _ := NewChatMessage("message 2")
+	m2.AssignID()
+	r.Add(m2)
+
+	// reply to the oldest message still in the list
+	m3, _ := m0.Reply("message 3")
+	m3.AssignID()
+	r.Add(m3)
+	g.Expect(r.Data()).Should(gomega.ContainElement(m3.UUID))
+	g.Expect(r.Data()).ShouldNot(gomega.ContainElement(m0.UUID))
+
+	// add a new message to the list
+	m4, _ := NewChatMessage("message 4")
+	m4.AssignID()
+	r.Add(m4)
+	g.Expect(r.Data()).ShouldNot(gomega.ContainElement(m1.UUID))
+
+	g.Expect(r.Data()).Should(gomega.ContainElement(m2.UUID))
+	g.Expect(r.Data()).Should(gomega.ContainElement(m3.UUID))
+	g.Expect(r.Data()).Should(gomega.ContainElement(m4.UUID))
 }
